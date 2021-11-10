f66fcf19028e9845326ae6a36c1ce50a8d7757dc
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2021-11-10 08:15:03 +0100

    cleanup

--- apps/playground/src/app/app.component.ts
+++ apps/playground/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { Subscription } from 'rxjs';
-import { Firestore } from './firestore.service';
+
 
 @Component({
   selector: 'ngfire-root',
@@ -9,19 +7,5 @@ import { Firestore } from './firestore.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  sub?: Subscription;
-  form = new FormGroup({ data: new FormControl() })
-  data$ = this.firestore.valueChanges('tests', ref => ref.where('data', '==', 'name'));
-  constructor(private firestore: Firestore) {
-    this.sub = this.data$.subscribe(console.log);
-    this.firestore.getValue('tests/aQ7DafGYHyoAlrSUPsAT', { expands: ['ref', 'array'] }).then(console.log);
-  }
 
-  unsubscribe() {
-    this.sub?.unsubscribe();
-  }
-
-  save(data: any) {
-    this.firestore.setDoc('tests', data);
-  }
 }
